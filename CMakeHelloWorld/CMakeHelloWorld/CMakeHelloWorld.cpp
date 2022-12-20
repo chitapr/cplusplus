a7e1d7d20a0816a80dbb4492bafe3561cfaf14ef
chitapr <marommv9@gmail.com>
2022-12-20 16:48:19 +0300

    cplusplus

---
+++ CMakeHelloWorld/CMakeHelloWorld/CMakeHelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeHelloWorld.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeHelloWorld.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
